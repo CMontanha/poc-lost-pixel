4dc94f2a17678cf1b61b837ff557f8ce20bf2fbd
CM <carlosmontanha@gmail.com>
2023-09-20 15:10:05 +0100

    adds test

--- test/specs/form.spec.ts
+++ test/specs/form.spec.ts
@@ -1,23 +1,30 @@
-import FormPage from '../pageobjects/form.page.js'
+import FormPage from "../pageobjects/form.page.js";
 
-describe('auth form', () => {
-    it('should deny access with wrong creds', async () => {
-        await FormPage.open()
-        await FormPage.username.setValue('foo')
-        await FormPage.password.setValue('bar')
-        await FormPage.submit()
+describe("auth form", () => {
+    it("should deny access with wrong creds", async () => {
+        await FormPage.open();
+        await FormPage.username.setValue("foo");
+        await FormPage.password.setValue("bar");
+        await FormPage.submit();
 
-        await FormPage.flash.waitForDisplayed()
-        await expect(FormPage.flash).toHaveTextContaining('Your username is invalid!')
-    })
+        await FormPage.flash.waitForDisplayed();
+        await FormPage.flash.saveScreenshot("./lost-pixel/invalidUsername.png");
+        await expect(FormPage.flash).toHaveTextContaining(
+            "Your username is invalid!"
+        );
+    });
 
-    it('should allow access with correct creds', async () => {
-        await FormPage.open()
-        await FormPage.username.setValue('tomsmith')
-        await FormPage.password.setValue('SuperSecretPassword!')
-        await FormPage.submit()
+    it("should allow access with correct creds", async () => {
+        await FormPage.open();
+        await FormPage.username.setValue("tomsmith");
+        await FormPage.password.setValue("SuperSecretPassword!");
+        await FormPage.password.saveScreenshot("./lost-pixel/password.png");
+        await FormPage.submit();
 
-        await FormPage.flash.waitForDisplayed()
-        await expect(FormPage.flash).toHaveTextContaining('You logged into a secure area!')
-    })
-})
+        await FormPage.flash.waitForDisplayed();
+        await FormPage.flash.saveScreenshot("./lost-pixel/loggedIn.png");
+        await expect(FormPage.flash).toHaveTextContaining(
+            "You logged into a secure area!"
+        );
+    });
+});

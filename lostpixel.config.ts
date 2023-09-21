05eb0ed9398a9f9a0cdf3fafe99fffb1603b9c75
CM <carlosmontanha@gmail.com>
2023-09-21 15:55:09 +0100

    fix GHA LP version

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -1,10 +1,10 @@
 import { CustomProjectConfig } from "lost-pixel";
 
 export const config: CustomProjectConfig = {
-    pageShots: {
-        pages: [{ path: "/", name: "landing" }],
-        baseUrl: "http://172.17.0.1:3000",
-    },
+    // pageShots: {
+    //     pages: [{ path: "/", name: "landing" }],
+    //     baseUrl: "http://172.17.0.1:3000",
+    // },
     lostPixelProjectId: "clmrneb1b6nzmj40ee4p7x86h",
     apiKey: process.env.LOST_PIXEL_API_KEY,
 };

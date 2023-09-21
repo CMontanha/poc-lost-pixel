5117300de63f0859f3291e391cebcf597f1a1e98
CM <carlosmontanha@gmail.com>
2023-09-21 09:59:37 +0100

    c

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -3,7 +3,7 @@ import { CustomProjectConfig } from "lost-pixel";
 export const config: CustomProjectConfig = {
     pageShots: {
         pages: [{ path: "/", name: "landing" }],
-        baseUrl: "http://172.17.0.1:3000",
+        // baseUrl: "http://172.17.0.1:3000",
     },
     lostPixelProjectId: "clmrneb1b6nzmj40ee4p7x86h",
     apiKey: process.env.LOST_PIXEL_API_KEY,
